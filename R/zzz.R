256bf081319feaeb75da962c68ea175233a01917
lshep <lori.shepherd@roswellpark.org>
2022-01-07 09:01:30 -0500

    User requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -8,4 +8,8 @@
         .Platform$GUI == "Rgui") {
         Biobase::addVigs2WinMenu("ProteomicsAnnotationHubData")
     }
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.16")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
